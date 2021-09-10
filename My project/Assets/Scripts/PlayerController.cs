bd07329fcc778aed5d046a5c9eb53be6b54b51ed
Sumit Kumar <kumarsumitjat298@gmail.com>
2021-09-10 11:11:55 +0530

    Added NewInput System and Update Player Controller

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,17 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private Rigidbody rb_Player;
     [SerializeField] private Camera cam;
+    [SerializeField] private float xMoveSpeed=10.0f;
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        float x_input = Input.GetAxis("Horizontal")*xMoveSpeed;
+        rb_Player.transform.Translate(x_input, 0, 0);
+
     }
 }
