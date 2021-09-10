7b81241067c34ae81f19620cfaf170415a382c83
Sumit Kumar <kumarsumitjat298@gmail.com>
2021-09-10 10:42:43 +0530

    Created GameManager Script and PlayerController Script

---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Rigidbody rb_Player;
+    [SerializeField] private Camera cam;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GoGoal
-This is a unity3D - Android game
+-This is a unity3D - Android game
+-This game is based on _Ball in a Maze_.
