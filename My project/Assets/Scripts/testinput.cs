bb8cfb97ca62d200d851e4da106f5be4c5f5d8d1
Sumit Kumar <kumarsumitjat298@gmail.com>
2021-09-23 14:49:56 +0530

    added a test scene to test new input system

---
+++ My project/Assets/Scripts/testinput.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class testinput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
