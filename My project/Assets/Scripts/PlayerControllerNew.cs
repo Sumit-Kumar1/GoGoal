1aceec48f10714d17a8674ade27002feae76e36a
Sumit Kumar <kumarsumitjat298@gmail.com>
2021-09-29 22:08:48 +0530

    Setup New input system for movement input

--- My project/Assets/Scripts/PlayerControllerNew.cs
+++ My project/Assets/Scripts/PlayerControllerNew.cs
@@ -1,3 +1,5 @@
+using System.Security.AccessControl;
+using System.Runtime.InteropServices.ComTypes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +8,13 @@ public class PlayerControllerNew : MonoBehaviour
 {
     // Start is called before the first frame update
     InputActionsAsset controls;
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private Rigidbody _rigidbody;
+
     private void Awake()
     {
         controls = new InputActionsAsset();//Initialize the new inputActionMap object
-        controls.Player.Move.performed += ctx => Move();
+        controls.Player.Move.performed += ctx => Move(controls.Player.Move.ReadValue<Vector2>());
     }
     private void OnEnable()
     {
@@ -18,5 +23,15 @@ public class PlayerControllerNew : MonoBehaviour
     private void OnDisable()
     {
         controls.Player.Disable();
+    }
+    private void Start()
+    {
+        _rigidbody = this.GetComponent<Rigidbody>();
+    }
+    void Move(Vector2 inputs)
+    {
+        Vector3 movement = new Vector3(inputs.x, 0.0f, inputs.y);
+        movement *= moveSpeed * Time.deltaTime;
+        _rigidbody.AddForce(movement, ForceMode.VelocityChange);
     }
 }
